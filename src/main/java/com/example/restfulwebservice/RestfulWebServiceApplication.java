257325ff16c6b1052dbf30d02915d5e254daf56d
shshhan <seunghyeonhan95@gmail.com>
2022-04-16 21:58:20 +0900

    Internationalization

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,6 +2,11 @@ package com.example.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -10,4 +15,12 @@ public class RestfulWebServiceApplication {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
     }
 
+    @Bean   //springboot가 초괴화될 때 해당 정보가 메모리에 올라가게 되어 다른 곳에서 사용 가능
+    public LocaleResolver localResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+
+        return localeResolver;
+
+    }
 }
--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.example.restfulwebservice.helloworld;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
+@RequiredArgsConstructor
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
 
     // GET
     // /hello-world (endPoint)
@@ -29,6 +37,13 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable(value = "name") String name){    //만약 pathVariable의 이름과 매개변수의 이름이 다르면 value속성으로 pathVariable의 이름을 지정
         return new HelloWorldBean(String.format("Hello World, %s", name));
         //String.format(표현하고자 하는 문자열 형태, 가변 데이터의 값)
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name="Accept-Language", required=false) Locale locale){  //@RequestHeader : 리퀘스트 헤더에 의해서 전달 되는 값
+
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 
 }
